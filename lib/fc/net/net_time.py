37a52197e9548d9c0a08194b69970959ef88616b
Fred Christianson <fred@devrelief.com>
2025-02-13 17:46:41 -0600

    simplified datetime.  old version was killing wifi for some reason

--- lib/fc/net/net_time.py
+++ lib/fc/net/net_time.py
@@ -1,27 +1,27 @@
-
-        
-        
+      
 import urequests
-from datetime import datetime, timedelta, tzinfo,timezone
 import ntptime
-
+from datetime import datetime,timezone,timedelta
 import logging
 log = logging.getLogger("fc.time")
 
 def get_timedelta(offset):
     if type(offset) == int:
-        return timedelta(minutes=offset)
+        return timedelta(minutes = offset)
     elif type(offset) == float:
-        return timedelta(hours=int(offset),minutes=int(offset*100%60))
-    elif type(offset) == str:
-        hour_min = [int(v) for v in offset.split(':')]
-        hour = hour_min[0]
-        min = hour_min[1] if hour >= 0 else -hour_min[1]
-        return timedelta(hours=hour,minutes= min)
-    else:
-        log.error(f"unknown timedelta offset {offset}")
-        return timedelta(0)
+        hours = int(offset)
+        minutes = int((offset-hours)*100)
+        return timedelta(hours =hours,minutes = minutes)
+    elif type(offset)  == str:
+        hm = offset.split(':')
+        hours = int(hm[0])
+        sign = 1 if hours>=0 else -1
+        minutes = sign * int(hm[1]) if len(hm)>1 else 0
+        return timedelta(hours =hours,minutes = minutes)
         
+        
+        
+
 class NetTime:
     is_dst = None
     dst_offset = None
@@ -31,7 +31,7 @@ class NetTime:
     @classmethod
     async def update(cls):
         log.info("update time")
-        if timezone is None:
+        if cls.tz is None:
            await cls.update_timezone()
         ntptime.settime()
         now = datetime.now()
@@ -44,13 +44,12 @@ class NetTime:
     async def update_timezone(cls):
         resp = urequests.get('http://worldtimeapi.org/api/ip') 
         if resp.status_code == 200:
-            log.info(f"worldtime: {resp.json()}")
             wtime = resp.json()
             cls.is_dst = wtime['dst']
-            cls.dst_offset = timedelta(hours=1) if cls.is_dst else timedelta(days=0)
+            cls.dst_offset = timedelta(hours=1) if cls.is_dst else timedelta(hours=0)
             cls.gmt_offset = get_timedelta(wtime['utc_offset'])
-            log.info(f"Timezone:  dst={cls.is_dst}  dst_offset={cls.dst_offset.isoformat()}  gmt_offset={cls.gmt_offset().iso_format()}")
-            tz = timezone(cls.dst_offset+cls.gmt_offset,wtime['timezone'])
+            log.info(f"Timezone:  dst={cls.is_dst}  dst_offset={cls.dst_offset.isoformat()}  gmt_offset={cls.gmt_offset.isoformat()}")
+            tz = timezone(cls.gmt_offset,cls.is_dst,wtime['timezone'],wtime['abbreviation'],cls.dst_offset)
             timezone.set_default(tz)
             cls.tz = tz
         else:
